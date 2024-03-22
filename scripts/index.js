1837bca0d261d09c734a5d442dc060e9d49e774b
Dmitriy Baikin <min-di@mail.ru>
2024-03-22 21:43:46 +0700

    работа выполнена

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
-
+const cardBox = document.querySelector(".places__list");
 // @todo: Функция создания карточки
-
+function createCard(cardContent, cardDelete) {
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
+  const cardDeleteButton = cardElement.querySelector(".card__delete-button");
+  cardElement.querySelector(".card__title").textContent = cardContent.name;
+  cardElement.querySelector(".card__image").src = cardContent.link;
+  cardDeleteButton.addEventListener("click", function () {
+    cardDelete();
+  });
+  return cardElement;
+}
 // @todo: Функция удаления карточки
-
+function cardDelete() {
+  const cardElement = document.querySelector(".places__item");
+  cardElement.remove();
+}
 // @todo: Вывести карточки на страницу
+function addCards() {
+  initialCards.forEach((cardContent) =>
+    cardBox.append(createCard(cardContent, cardDelete))
+  );
+}
+addCards();
